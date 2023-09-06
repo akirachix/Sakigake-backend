b6c1c04f8a69221a6bcbe7dc7aa0f18200eda003
Kevine-nzayanga <kevinenzayanga@gmail.com>
2023-09-06 11:01:31 +0300

    Configured postgres

--- sakigakebackendproject/sakigakebackendproject/settings.py
+++ sakigakebackendproject/sakigakebackendproject/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'sakigakebackendproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'mzaziconnect',      
         'USER': 'sakigake',        
         'PASSWORD': 'mzaziconnect',  
