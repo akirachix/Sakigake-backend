f1213d37ae8b893c0d5f35fcb6aee9c8d410b972
Kevine-nzayanga <kevinenzayanga@gmail.com>
2023-09-07 22:09:58 +0300

    added restframework

--- sakigakebackendproject/sakigakebackendproject/settings.py
+++ sakigakebackendproject/sakigakebackendproject/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'comments',
+    'comments',  
+    'rest_framework',
+    'api',
 ]
 
 MIDDLEWARE = [
