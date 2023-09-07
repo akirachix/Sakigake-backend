5eca1e2909081e3fb91f3211e4fef7ca8de3acb9
Sonia-Uwamahoro <soniauwamahooro@gmail.com>
2023-09-07 22:08:21 +0300

    updated feature/teachers-subjects

--- sakigakebackendproject/teachers/models.py
+++ sakigakebackendproject/teachers/models.py
@@ -1,19 +1,16 @@
 from django.db import models
-from .models import Subjects
 
-class Teachers(models.Model):
+class Teacher(models.Model):
     school = models.ForeignKey('School', on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
     email = models.EmailField()
-    phone_number = models.CharField(max_length=15)
-    # subjects = models.ManyToManyField(Subjects) 
-    subjects = models.ManyToManyField(Subjects)
-    grades = models.JSONField()
-    password = models.CharField(max_length=128)
-    is_class_teacher = models.BooleanField(default=False) # Indicates whether the teacher is a class teacher
+    phone_number = models.CharField(max_length=20)
+    subjects = models.ManyToManyField('Subject')
+    grades = models.ManyToManyField('Grade')
+    password = models.CharField(max_length=255)
     date_added = models.DateTimeField(auto_now_add=True)
-    date_updated = models.DateTimeField(auto_now_add=True)
-
-def __str__(self):
-      return f"{self.first_name} {self.last_name}"
+    date_updated = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
