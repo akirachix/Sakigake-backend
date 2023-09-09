2d75942f5437d9c2169a05c6bf67cb8d0a15dc5d
Florence-nyokabi <florencenyokabiwangui@gmail.com>
2023-09-09 12:52:39 +0300

    Added phone number field

--- sakigakebackendproject/students/models.py
+++ sakigakebackendproject/students/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 class Student(models.Model):
     first_name = models.CharField(max_length=32)
     last_name = models.CharField(max_length=32)
-    parent_phone_number =models.PhoneNumberField(blank=True, null=True)
+    parent_phone_number = models.PhoneNumberField(blank=True, null=True)
     date_added = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
     class_grade = models.CharField(max_length=32)
