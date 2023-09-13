a50322ed6d530a2115cf8c0227cc4dd68fb1b508
Sonia-Uwamahoro <soniauwamahooro@gmail.com>
2023-09-13 16:26:08 +0300

    added unit test for both teacher and subject models

--- sakigakebackendproject/teachers/tests.py
+++ sakigakebackendproject/teachers/tests.py
@@ -1,3 +1,60 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_framework.test import APIRequestFactory
+from .models import Teacher
+from .serializers import TeacherSerializer
+from .views import TeacherListView, TeacherDetailView
 
-# Create your tests here.
+
+class TeacherListViewTestCase(TestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.view = TeacherListView.as_view()
+        self.url = '/teachers/'
+
+    def test_get_teachers(self):
+        Teacher.objects.create(first_name='Alice', last_name='Warter', email='alicewarter@gmail.com')
+        Teacher.objects.create(first_name='John', last_name='Kimani', email='johnkimani@gmail.com')
+
+        request = self.factory.get(self.url)
+        response = self.view(request)
+
+        teachers = Teacher.objects.all()
+        serializer = TeacherSerializer(teachers, many=True)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_create_teacher(self):
+        data = {
+            'first_name': 'Dave',
+            'last_name': 'Kipepeo',
+            'email': 'kipepeodave@gmail.com',
+            'phone_number': '+123456789',
+            'password': '23',
+            'is_class_teacher': False
+        }
+
+        request = self.factory.post(self.url, data)
+        response = self.view(request)
+
+       
+
+
+class TeacherDetailViewTestCase(TestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.view = TeacherDetailView.as_view()
+        self.url = '/teachers/{}/'
+
+    def test_get_teacher(self):
+        teacher = Teacher.objects.create(first_name='John', last_name='Doe', email='john@example.com')
+        url = self.url.format(teacher.id)
+
+        request = self.factory.get(url)
+        response = self.view(request, id=teacher.id)
+
+        serializer = TeacherSerializer(teacher)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
--- sakigakebackendproject/teachers/urls.py
+++ sakigakebackendproject/teachers/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from . import views
 
 from.views import TeacherListView,TeacherDetailView
 urlpatterns=[
     path("teachersList/", TeacherListView.as_view(),name="teachers_list_view"),
     path("teachersDetail/<int:id>/", TeacherDetailView.as_view(),name="teachers_detail_view"),
+    # path('login/', views.teacher_login, name='teacher_login'),
+    # path('logout/', views.teacher_logout, name='teacher_logout'),
+    # path('teachers/<int:id>/change-password/', views.TeacherDetailView.as_view(), name='teacher_change_password'),
 ]
