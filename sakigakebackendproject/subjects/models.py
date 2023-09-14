ccc99028242524fdd0804214a063f0cf162ad8cd
Sonia-Uwamahoro <soniauwamahooro@gmail.com>
2023-09-14 21:32:42 +0300

    added a .env file

--- sakigakebackendproject/subjects/models.py
+++ sakigakebackendproject/subjects/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Subject(models.Model):
     
-    subject_name = models.CharField(max_length=255)
+    subject_name = models.CharField(max_length=255, unique=True)
     description = models.TextField(blank=True)
 
     def __str__(self):
