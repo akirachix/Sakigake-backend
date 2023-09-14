dbb379d3152cf8384c8c7422e46bd256c922f0d9
Sonia-Uwamahoro <soniauwamahooro@gmail.com>
2023-09-14 18:19:39 +0300

    added more tests

--- sakigakebackendproject/teachers/tests.py
+++ sakigakebackendproject/teachers/tests.py
@@ -38,7 +38,42 @@ class TeacherListViewTestCase(TestCase):
         request = self.factory.post(self.url, data)
         response = self.view(request)
 
-       
+    def test_create_teacher_invalid_data(self):
+        # Test creating a teacher with invalid data, which should return a 400 BAD REQUEST status code.
+        invalid_data = {
+            'first_name': 'Invalid',  # Missing 'email' field
+        }
+
+        request = self.factory.post(self.url, invalid_data)
+        response = self.view(request)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_update_teacher(self):
+        # Test updating a teacher's information.
+        teacher = Teacher.objects.create(first_name='Alice', last_name='Warter', email='alicewarter@gmail.com')
+        url = self.url.format(teacher.id)
+
+        updated_data = {
+            'first_name': 'Updated Alice',
+            'last_name': 'Updated Warter',
+            'email': 'updated_email@gmail.com',
+        }
+
+        request = self.factory.put(url, updated_data)
+        response = self.view(request, id=teacher.id)
+
+        teacher.refresh_from_db()
+        serializer = TeacherSerializer(teacher)
+
+
+    def test_delete_teacher(self):
+        # Test deleting a teacher.
+        teacher = Teacher.objects.create(first_name='John', last_name='Doe', email='john@example.com')
+        url = self.url.format(teacher.id)
+
+        request = self.factory.delete(url)
+        response = self.view(request, id=teacher.id)
 
 
 class TeacherDetailViewTestCase(TestCase):
