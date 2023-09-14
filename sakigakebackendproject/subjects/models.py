954f28ec641bb2130400c79833d2c60db0d910af
Sonia-Uwamahoro <soniauwamahooro@gmail.com>
2023-09-14 13:24:22 +0300

    updated subjects model

--- sakigakebackendproject/subjects/models.py
+++ sakigakebackendproject/subjects/models.py
@@ -1,24 +1,9 @@
 from django.db import models
-# from .models import Teacher
-# from .models import Grade
-
 
 class Subject(models.Model):
-    CHOICES = (
-        ('1', 'English'),
-        ('2', 'Kiswahili'),
-        ('3', 'Mathematics'),
-        ('4', 'Integrated Science'),
-        ('5', 'Social Studies'),
-        ('6', 'Business Studies'),
-        ('7', 'Agriculture'),
-        ('8', 'Pre-technical and Pre-Career Studies'),
-        ('9', 'Religious Studies Education'),
-    )
-    subject_name = models.CharField(max_length=100, choices=CHOICES)
+    
+    subject_name = models.CharField(max_length=255)
     description = models.TextField(blank=True)
-    # teachers = models.ManyToManyField('Teacher', related_name='subjects_taught', blank=True)
-    # grades = models.ManyToManyField('Grade')
 
     def __str__(self):
         return self.subject_name
