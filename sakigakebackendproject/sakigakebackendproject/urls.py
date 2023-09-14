dcc65a2d3b3a3e6d0538440716d1bcabd19ddccf
Sonia-Uwamahoro <soniauwamahooro@gmail.com>
2023-09-14 23:10:01 +0300

    resolved conflicts

--- sakigakebackendproject/sakigakebackendproject/urls.py
+++ sakigakebackendproject/sakigakebackendproject/urls.py
@@ -36,11 +36,11 @@ urlpatterns = [
          name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
          name='schema-redoc'),
-]
-
     path("comments/", include ("comments.urls")),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-
-
 ]
+
+  
+
+
+
 
