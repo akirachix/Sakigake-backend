80c17ea19438b856fd2f335203866277dd75ef5c
Nakakandebr <bridgetnakakandea@gmail.com>
2023-09-14 22:11:42 +0300

    added permissions

--- sakigakebackendproject/accounts/permissions.py
+++ sakigakebackendproject/accounts/permissions.py
@@ -1,57 +1,19 @@
-# from rest_framework import permissions
-# class IsAdminOrReadOnly(permissions.BasePermission):
-#     def has_permission(self, request, view):
-#         if request.method in permissions.SAFE_METHODS:
-#             return True
-#         return request.user and request.user.is_staff
-    
-
-
-# class IsOwnerOrAdmin(permissions.BasePermission):
-#     def has_object_permission(self, request, view, obj):
-#         if request.user and request.user.is_staff:
-#             return True
-#         if hasattr(obj, 'created_by'):
-#             return obj.created_by == request.user
-#         return False
-    
-
-
 from rest_framework import permissions
-
 class IsAdminOrReadOnly(permissions.BasePermission):
-    """
-    Permission to allow administrators full access,
-    but read-only access for others (including Teachers and schools).
-    """
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
         return request.user and request.user.is_staff
+    
+
 
 class IsOwnerOrAdmin(permissions.BasePermission):
-    """
-    Permission to allow administrators full access,
-    object owners access, and read-only access for others.
-    """
     def has_object_permission(self, request, view, obj):
-        if request.user and (request.user.is_staff or obj.created_by == request.user):
+        if request.user and request.user.is_staff:
             return True
+        if hasattr(obj, 'created_by'):
+            return obj.created_by == request.user
         return False
+    
 
-class IsAdminOrNGO(permissions.BasePermission):
-    """
-    Custom permission for administrators and Schools (CustomUsers).
-    """
-    def has_object_permission(self, request, view, obj):
-        print(f"::::::::::::::::::::::::::::::::::::{request.user}=============")
-        if request.user and (request.user.is_staff or obj.created_by == request.user):
-            return True
-        return False
 
-class IsHealthworker(permissions.BasePermission):
-    """
-    Custom permission for Schools.
-    """
-    def has_permission(self, request, view):
-        return getattr(request.user, 'is_teacher',True)
--- sakigakebackendproject/accounts/urls.py
+++ sakigakebackendproject/accounts/urls.py
@@ -1,26 +1,10 @@
-# from django.urls import path
-# from .views import SignupView, LoginView, LogoutView, UserListAPIView,UserDetailAPIView
-
-# urlpatterns = [
-#     path('signup/', SignupView.as_view(), name='signup'),
-#     path('login/', LoginView.as_view(), name='login'),
-#     path('logout/', LogoutView.as_view(), name='logout'),
-#     path('users/', UserListAPIView.as_view(), name='user-list'),
-#     path('users/<int:pk>/', UserDetailAPIView.as_view(), name='user-detail'),
-# ]
-
 from django.urls import path
-from . import views
-​
+from .views import SignupView, LoginView, LogoutView, UserListAPIView,UserDetailAPIView
+
 urlpatterns = [
-    path('ngo/signup/', views.ngo_signup, name='ngo-signup'),
-    path('ngo/logout/', views.ngo_logout, name='ngo-logout'),
-    path('customusers/', views.CustomUserList.as_view(), name='customuser-list'),
-    path('customusers/<int:pk>/', views.CustomUserDetail.as_view(), name='customuser-detail'),
-    path('healthworkers/', views.HealthworkerList.as_view(), name='healthworker-list'),
-    path('healthworkers/<int:pk>/', views.healthworker_detail, name='healthworker-detail'),
-    path('healthworker/signup/', views.healthworker_signup, name='healthworker-signup'),
-    path('healthworker/logout/', views.healthworker_logout, name='healthworker-logout'),
-    path('healthworker/login/', views.healthworker_login, name='healthworker-login'),
-    path('ngo/login/', views.ngo_login, name='ngo-login'),
+    path('signup/', SignupView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('users/', UserListAPIView.as_view(), name='user-list'),
+    path('users/<int:pk>/', UserDetailAPIView.as_view(), name='user-detail'),
 ]
