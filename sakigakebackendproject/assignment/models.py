d630a5007acfc7f688e1096847dd0650a89a7c4d
Joan-Njiru <joanwanjirunjiru@example.com>
2023-09-14 22:46:09 +0300

    added assignment and shops

--- sakigakebackendproject/assignment/models.py
+++ sakigakebackendproject/assignment/models.py
@@ -2,9 +2,7 @@ from django.db import models
 from django.contrib.postgres.fields import ArrayField
 from firebase_admin import messaging
 
-from shop.models import Shop
-
-
+from shop.models import Category
 
 # Create your models here.
 class Assignment(models.Model):
@@ -15,24 +13,24 @@ class Assignment(models.Model):
     competency = models.CharField(max_length=100)
     task = models.TextField()
     materials = ArrayField(models.CharField(max_length=50))
-    category = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     due_date = models.DateTimeField(default=None, null=True, blank=True)
     date_added_at = models.DateTimeField(default=None, null=True, blank=True)
     date_updated_at = models.DateTimeField(default=None, null=True, blank=True)
 
-    def send_push_notification(self):
-        message = messaging.Message(
-            notification = message.Notification(
-                title = {self.topic},
-                body = {self.task},
-            ),
-            topic = "assignments"
-        )
-        response = messaging.send(message)
-        print("Successfully sent notification:", response)
-    def save(self):
-     super().save()
-     self.send_push_notification()
+    # def send_push_notification(self):
+    #     message = messaging.Message(
+    #         notification = message.Notification(
+    #             title = {self.topic},
+    #             body = {self.task},
+    #         ),
+    #         topic = "assignments"
+    #     )
+    #     response = messaging.send(message)
+    #     print("Successfully sent notification:", response)
+    # def save(self):
+    #  super().save()
+    #  self.send_push_notification()
 
 
     def __str__(self):
