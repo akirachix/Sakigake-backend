fd4cb62cfff580ea74fd3905292029cb6fb31832
sakigake <mzaziconnect@gmail.com>
2023-09-18 15:56:12 +0300

    changed commentor field in comments

--- comments/models.py
+++ comments/models.py
@@ -1,14 +1,12 @@
 from django.db import models
-from accounts.views import UserListAPIView
 from assignment.models import Assignment
 from django.conf import settings
 from parents.models import Parent
-# from django.contrib.auth.models import User
 
 
 
 class Comment(models.Model):
-    assignment = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,null=True)
+    assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE,null=True)
     commentor = models.ForeignKey(Parent, on_delete=models.CASCADE)
     content = models.TextField()   
     parent_comment = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='replies')
