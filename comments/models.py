72e6ab47e7ba768affb9804dbb321b7340add59d
sakigake <mzaziconnect@gmail.com>
2023-09-18 08:47:43 +0300

    changed user access

--- comments/models.py
+++ comments/models.py
@@ -11,10 +11,7 @@ class Comment(models.Model):
     content = models.TextField()   
     parent_comment = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='replies')
     created_at = models.DateTimeField(auto_now_add=True)  
-    updated_at = models.DateTimeField(auto_now=True)
     
-    class Meta:
-        ordering = ['updated_at']
 
     def __str__(self):
         return f"{self.commentor} - {self.created_at}"
