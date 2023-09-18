6d8bc02d449843cc12f4ca4c6fede371856032fe
sakigake <mzaziconnect@gmail.com>
2023-09-18 08:56:33 +0300

    made migrations

--- comments/admin.py
+++ comments/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Comment
 
 class CommentAdmin(admin.ModelAdmin):
-        list_display = ('commentor', 'content', 'created_at')
+        list_display = ('commentor', 'content', 'created_at', 'updated_at')
         date_hierarchy = 'created_at'
 
 
--- comments/models.py
+++ comments/models.py
@@ -7,10 +7,11 @@ from django.contrib.auth.models import User
 
 class Comment(models.Model):
     assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE,null=True)
-    commentor = models.ForeignKey(User, on_delete=models.CASCADE)
+    commentor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content = models.TextField()   
     parent_comment = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='replies')
     created_at = models.DateTimeField(auto_now_add=True)  
+    updated_at = models.DateTimeField(auto_now=True)
     
 
     def __str__(self):
