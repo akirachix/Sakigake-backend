1d5e0fd2626ba27627da578d9345da6abd32d922
sakigake <mzaziconnect@gmail.com>
2023-09-18 14:22:53 +0300

    added phone_number field in shop model

--- shop/admin.py
+++ shop/admin.py
@@ -4,6 +4,6 @@ from shop.models import Shop
 
 # Register your models here.
 class ShopAdmin(admin.ModelAdmin):
-    list_display=('name','location','category')
+    list_display=('name','location','phone_number','category')
 
 admin.site.register(Shop,ShopAdmin)
