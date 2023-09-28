779af71201072a2f4f8636b02a4da894d156fc1d
Florence-nyokabi <florencenyokabiwangui@gmail.com>
2023-09-28 08:37:27 +0300

    Updated comments

--- comments/models.py
+++ comments/models.py
@@ -2,12 +2,14 @@ from django.db import models
 from assignment.models import Assignment
 from django.conf import settings
 from parents.models import Parent
+from django.contrib.auth.models import CustomUser
+
 
 
 
 class Comment(models.Model):
     assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE,null=True)
-    commentor = models.ForeignKey(Parent, on_delete=models.CASCADE)
+    commentor = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     content = models.TextField()   
     parent_comment = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE, related_name='replies')
     created_at = models.DateTimeField(auto_now_add=True)  
