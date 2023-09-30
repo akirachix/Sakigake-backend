a77de4277c487e4a244b9fe50f582e4736ae9da5
sakigake <mzaziconnect@gmail.com>
2023-09-30 14:59:47 +0300

    added assign post

--- students/models.py
+++ students/models.py
@@ -9,7 +9,6 @@ class Student(models.Model):
     first_name = models.CharField(max_length=32)
     last_name = models.CharField(max_length=32)
     admission_number = models.CharField(max_length=10, unique=True)
-    parent_phone_number = PhoneNumberField(region='KE')
     date_added_at = models.DateTimeField(auto_now_add=True)
     date_updated_at= models.DateTimeField(auto_now=True)
     class_grade = models.ForeignKey(Grade, on_delete=models.CASCADE)
